eaac3c690a0497c59692dd52702eba12af2e2a3a
MO ZHAO <morrisz@student.ubc.ca>
2022-11-22 20:09:45 -0800

    setup script string (usage)

---
+++ src/data_processing.py
@@ -0,0 +1,16 @@
+# Author: Morris Zhao
+# date: 2022-11-23
+"""
+Cleaning and splitting the raw data into cleaned_train and cleaned_test data set and save to file path as csv file.
+
+Usage:
+  data_cleaning_spliting.py --input_path=<input_path> --out_dir=<out_dir>
+
+Options:
+  --input_path=<input_path>        Path (file path) to raw data (support csv file only)
+  --out_dir=<out_dir>              Path (directory) to save processed train and test data sets and cleaned data
+
+Example:
+From the root of the repository, the below command could be used to save files to data/processed folder:
+python ./src/data_cleaning_spliting.py --input_path="./data/raw/credit_default_data.csv" --out_dir="./data/processed"
+"""
