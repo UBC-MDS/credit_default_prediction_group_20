c53335e0993dd752ffb59274a7fd0f88eb22881b
Ken Wang <kenuiuc@gmail.com>
2022-11-24 20:07:44 -0800

    working on getting test f-1 score for all the models

--- src/model_summary.py
+++ src/model_summary.py
@@ -1,16 +1,15 @@
 # Example:
-# python model_summary.py --model_dir='../results/trained_models/svc.joblib'  --test_data='../data/processed/credit_test_df.csv' --output_dir=output_dir_value
+# python model_summary.py --model_dir='../results/trained_models'  --test_data='../data/processed/credit_test_df.csv' --output_dir=output_dir_value
 
 
 # author: Ken Wang
 # date: 2022-11-24
 
 """This script generate evaluation figures for a binary classification model.
-Usage: model_summary.py --model_dir=<model_dir>  --model_name=<model_name> --test_data=<test_data> --output_dir=<output_dir>
+Usage: model_summary.py --model_dir=<model_dir>  --test_data=<test_data> --output_dir=<output_dir>
 
 Options:
 --model_dir=<model_dir> folder with joblib model files on disk
---model_name=<model_name> the best model to evaluate, e.x `logistic_regression`
 --X_test=<X_test>  test feature data file in csv format
 --y_test=<y_test>  test target data file in csv format
 --output_dir=<output_dir>  output directory to put summary figures in
@@ -94,32 +93,42 @@ def load_model(model_path):
     model = joblib_load(model_path)
     return model
 
-def main(model_dir=None, model_name=None, test_data_path=None, output_dir_path=None):
-    model_path = os.path.join(model_dir, model_name + '.joblib')
-    model = load_model(model_path)
+
+def main(model_dir=None, test_data_path=None, output_dir_path=None):
     X_test, y_test = load_test_data(test_data_path)
-    y_hat_test = model.predict(X_test)
+    
+    # for all models plot the F-1 score
+    model_name_list = ['svc', 'logistic_regression', 'dummy_classifier', 'random_forest', 'knn']
+    for model_name in model_name_list:
+        print(f'\n---------- model_name = {model_name} ------------\n')
+        model_path = os.path.join(model_dir, model_name + '.joblib')
+        model = load_model(model_path)
 
-    print(f'y_hat_test = {y_hat_test}')
-    print(f'type(y_hat_test) = {type(y_hat_test)}')
-    print(f'y_hat_test.shape = {y_hat_test.shape}')
+        y_hat_test = model.predict(X_test)
+        print(f'y_hat_test = {y_hat_test}')
+        print(f'type(y_hat_test) = {type(y_hat_test)}')
+        print(f'y_hat_test.shape = {y_hat_test.shape}')
 
-    get_confusion_matrix(model, X_test, y_test, os.path.join(output_dir_path, model_name + '_confusion_matrix.png'))
+    # for the best model `logistic regression` make some figures
+    best_model_name = 'logistic_regression'
+    print(f'\n---------- best_model_name = {best_model_name} ------------\n')
+
+    get_classification_report(model, X_test, y_test, os.path.join(output_dir_path, best_model_name + '_classification_report.csv'))
+    print('saved classification report')
+    
+    get_confusion_matrix(model, X_test, y_test, os.path.join(output_dir_path, best_model_name + '_confusion_matrix.png'))
     print('saved confusion matrix png')
 
-    get_pr_curve(model, X_test, y_test, os.path.join(output_dir_path, model_name + '_precision_recall_curve.png'))
+    get_pr_curve(model, X_test, y_test, os.path.join(output_dir_path, best_model_name + '_precision_recall_curve.png'))
     print('saved precision recall curve png')
 
-    get_roc_auc(model, X_test, y_test, os.path.join(output_dir_path, model_name + '_roc_auc.png'))
+    get_roc_auc(model, X_test, y_test, os.path.join(output_dir_path, best_model_name + '_roc_auc.png'))
     print('saved ROC AUC png')
 
-    get_classification_report(model, X_test, y_test, os.path.join(output_dir_path, model_name + '_classification_report.csv'))
-    print('saved classification report')
 
 if __name__ == "__main__":
     main(
         model_dir=opt['--model_dir'],
-        model_name=opt['--model_name'],
         test_data_path=opt['--test_data'],
         output_dir_path=opt['--output_dir']
     )
